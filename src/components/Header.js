c7945411bd64cda3306ee744cc7e4b1fa15a8386
Emmanuel Adiba <eabaagah@gmail.com>
2021-01-27 09:54:20 +0000

    template of project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = ({ title }) => {
+    return (
+        <header className="header">
+            <h1>{ title }</h1>
+            <button className="btn">Add</button>
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+
+export default Header
