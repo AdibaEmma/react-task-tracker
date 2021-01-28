304088265a7daccae63a49f7f8747a64378b9ade
Emmanuel Adiba <eabaagah@gmail.com>
2021-01-28 11:37:55 +0000

    update on delete tasks

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={ deleteTask } />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask } /> : "No Tasks to Show" }
     </div>
   );
 }
