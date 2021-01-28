f148c0a8dc3f0faa755565c3de468a672f67eafe
Emmanuel Adiba <eabaagah@gmail.com>
2021-01-28 12:01:22 +0000

    toggle task reminder

--- src/App.js
+++ src/App.js
@@ -17,18 +17,36 @@ function App() {
       "text": "Meeting at School",
       "day": "Feb 6th at 1:30pm",
       "reminder": true
+    },
+    {
+      "id": 3,
+      "text": "Complete React Project",
+      "day": "Feb 20th at 07:00pm",
+      "reminder": false
     }
   ])
 
-
+// Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter(task => task.id !== id ))
   }
 
+// Toggle Reminder
+
+const toggleReminder = (id) => {
+  setTasks(
+     tasks.map(task => 
+       task.id === id ? {...task, reminder: 
+        !task.reminder} : task
+     )
+  )
+}
+
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask } /> : "No Tasks to Show" }
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask } onToggle={toggleReminder} /> : "No Tasks to Show" }
     </div>
   );
 }
