2cfd5f8462ba834f9ffd5b8290826103a1cd6499
Emmanuel Adiba <eabaagah@gmail.com>
2021-01-28 11:36:04 +0000

    delete task

--- src/App.js
+++ src/App.js
@@ -20,10 +20,15 @@ function App() {
     }
   ])
 
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id ))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={ deleteTask } />
     </div>
   );
 }
